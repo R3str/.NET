05678754c99d5619ea6b546804543f9be6e540bc
R3str <andrey.kotoman12@gmail.com>
2019-03-13 15:49:46 +0200

    1

---
+++ №1/№1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONPU.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+
+            Console.WriteLine("Введите целое число :");
+            n = Console.ReadLine();
+
+            bool check = int.TryParse(n, out int a);
+
+            if (check)
+            {
+                a = a * a;
+                Console.WriteLine("Результат: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректные данные");
+            }
+
+        }
+    }
+}
