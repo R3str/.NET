fc8e4d4e184dce8b7c16d272aa191d5f82118181
Андрей Котоман <andrey.kotoman12@gmail.com>
2019-09-23 22:48:13 +0300

    1

--- 2 course/№2/№2/Program.cs
+++ 2 course/№2/№2/Program.cs
@@ -11,32 +11,98 @@ namespace _2
     {
         static void Main(string[] args)
         {
-            string[] start = new string[3];
-            int index = 1, count = 0;
+            string[] start = new string[2];
+            string[] change = new string[2];
+            int count = 0;
             // Считывание файла
-            using (StreamReader SR = new StreamReader(@"E:\1Programming test\2 course\input.txt"))
+            using (StreamReader SR = new StreamReader(@"E:\_Programming test\2 course\input.txt"))
             {
                 start[0] = SR.ReadLine();
+                start[1] = SR.ReadLine();
                 SR.Close();
             }
             // Приветствие
-            Console.WriteLine("Вы запустили программу для вычитания двух чисел (каждое из которых не должно превышать 16-ую степень разрядности).");
+            Console.WriteLine("Вы запустили программу для вычитания двух чисел (каждое из которых не должно превышать 16-ую степень разрядности)." +
+                "\nВаши числа: " + start[0] + " и " + start[1]);
             // Проверка на соответствие разрядности
-            if (start[0].Length > 16)
+            if (start[0].Length > 16 || start[1].Length > 16)
             {
                 Console.WriteLine("\nВы ввели некорректное число! Попробуйте еще раз");
                 Environment.Exit(0);
             }
             // Проверка на корректность вводимого числа (отсутствие букв и недопустимых цифр)
-            for (int i = 0; i < start[0].Length; i++)
+            Check(start[0]);
+            Check(start[1]);
+            // Добавление нехватающих нулей для равной разрядности обеих чисел
+            if(start[0].Length > start[1].Length || start[0].Length < start[1].Length)
             {
-                if (start[0][i] != ',' && start[0][i] != '1' && start[0][i] != '0')
+                count = start[0].Length - start[1].Length;
+
+                if (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                        change[1] += "0";
+                    change[1] += start[1];
+                    change[0] = start[0];
+                }
+
+                if(count < 0)
+                {
+                    for (int i = 0; i > count; i--)
+                        change[0] += "0";
+                    change[0] += start[0];
+                    change[1] = start[1];
+                }
+            }
+            // Вычитание чисел
+            count = change[0].Length;
+            int num = 0;
+            start[0] = null;
+            bool minusOne = false;
+
+            for(int i = count-1; i >= 0; i--)
+            {
+                num = Convert.ToInt32(change[0][i]) - Convert.ToInt32(change[1][i]);
+                if (minusOne)
+                {
+                    num = --num;
+                    if (num == -2)
+                    {
+                        num = 0;
+                        minusOne = true;
+                    }
+                    else
+                        minusOne = false;
+                }
+                if (num < 0)
+                {
+                    minusOne = true;
+                    num = Math.Abs(num);
+                }
+
+                start[0] += num;
+            }
+
+            char[] reverseString = start[0].ToCharArray();
+            Array.Reverse(reverseString);
+            Console.WriteLine(reverseString);
+
+        }
+
+        static bool Check(string s1)
+        {
+            bool check = true;
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != '.' && s1[i] != ',' && s1[i] != '1' && s1[i] != '0')
                 {
                     Console.WriteLine("\nВы ввели некорректное число! Попробуйте еще раз");
                     Environment.Exit(0);
                 }
             }
 
+            return check;
         }
     }
 }
